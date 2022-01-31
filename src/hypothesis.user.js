158f8e0e2b58f01be4c9b564c047fd16e60010fc
abstraction <16391550+abstraction@users.noreply.github.com>
2022-01-31 13:38:58 +0530

    added button to trigger Hypothesis

--- src/hypothesis.user.js
+++ src/hypothesis.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
-// @name        Hypothesis
+// @name        Hypothesis 2
 // @namespace   https://github.com/abstraction/userscripts
 // @description Hypothesis annotator for Firefox and Firefox Android
 // @author      abstraction
-// @version     1.0.0
+// @version     1.1.0
 // @match       http*://*/*
 // @homepageURL https://github.com/abstraction/userscripts
 // @updateURL   https://github.com/abstraction/userscripts/blob/master/src/hypothesis.user.js
+// @grant       GM_addStyle
 // ==/UserScript==
 
-// modified header info to bypass csp (see notes) or use Tampermonkey
-
-(function() {
+const hypo = function() {
    window.hypothesisConfig = function() {
       return {
          showHighlights: true,
@@ -22,4 +21,44 @@
       s = d.createElement('script');
    s.setAttribute('src', 'https://hypothes.is/embed.js');
    d.body.appendChild(s)
-})();
+};
+
+const btn = document.createElement('button');
+btn.id = 'btn-hypo'
+btn.textContent = '🖊️'
+document.body.appendChild(btn);
+
+const getBtn = document.getElementById('btn-hypo');
+
+let listenHypo = getBtn.addEventListener('click', () => {
+  hypo();
+   getBtn.style.display = 'none';
+}, {once: true})
+
+//--- Style our newly added elements using CSS.
+GM_addStyle ( `
+#btn-hypo {
+ position: fixed;
+ top: 50px;
+ right: 10px;
+ background: #FBCA1F;
+ font-family: inherit;
+ padding: 0.1em 0.6em;
+ font-weight: 900;
+ font-size: 18px;
+ border: 1px solid black;
+ border-radius: 0.4em;
+ box-shadow: 0.1em 0.1em;
+ z-index: 9999;
+}
+
+#btn-hypo:hover {
+ transform: translate(-0.05em, -0.05em);
+ box-shadow: 0.15em 0.15em;
+}
+
+#btn-hypo:active {
+ transform: translate(0.05em, 0.05em);
+ box-shadow: 0.05em 0.05em;
+}
+` );
