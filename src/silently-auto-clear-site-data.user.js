c465db5183a3a952bbfc5e30a57dda523eb7bd86
abstraction <16391550+abstraction@users.noreply.github.com>
2024-07-02 14:18:13 +0530

    refactor code

--- src/silently-auto-clear-site-data.user.js
+++ src/silently-auto-clear-site-data.user.js
@@ -1,41 +1,35 @@
 // ==UserScript==
-// @name         Silent Auto Clear Site Data
-// @namespace    http://tampermonkey.net/
-// @version      2024-04-20
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.summarize.tech/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=summarize.tech
-// @grant        none
-// @updateURL    https://github.com/abstraction/userscripts/blob/master/src/silently-auto-clear-site-data.user.js
-// @downloadURL  https://github.com/abstraction/userscripts/blob/master/src/silently-auto-clear-site-data.user.js
+// @name           Silent Auto Clear Site Data
+// @version        2024-07-02
+// @description    Clean cookies and local storage
+// @author         abstraction
+// @match          https://www.summarize.tech/*
+// @icon           https://www.google.com/s2/favicons?sz=64&domain=summarize.tech
+// @grant          none
+// @homepageURL    https://github.com/abstraction/userscripts/
+// @updateURL      https://github.com/abstraction/userscripts/blob/master/src/silently-auto-clear-site-data.user.js
+// @downloadURL    https://github.com/abstraction/userscripts/blob/master/src/silently-auto-clear-site-data.user.js
 // ==/UserScript==
 
 (function () {
   'use strict';
 
   // Clear cookies and site data
-  window.addEventListener('load', function () {
-    // Clear all site storage if supported
+  window.addEventListener('load', () => {
     if ('storage' in navigator && 'estimate' in navigator.storage) {
       navigator.storage.estimate().then((estimate) => {
         if (estimate.usage > 0) {
           navigator.storage.clear().then(() => {
-            // eslint-disable-next-line no-console
             console.log('All site storage cleared.');
           });
         }
       });
     }
 
-    // Clear cookies
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i];
-      var eqPos = cookie.indexOf('=');
-      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-      document.cookie =
-        name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-    }
+    document.cookie.split(';').forEach(cookie => {
+      const eqPos = cookie.indexOf('=');
+      const name = eqPos > -1 ? cookie.slice(0, eqPos) : cookie;
+      document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+    });
   });
 })();
--- src/youtube-add-summary-btn.user.js
+++ src/youtube-add-summary-btn.user.js
@@ -1,67 +1,55 @@
 // ==UserScript==
-// @name         Add Summarize Button
-// @namespace    http://tampermonkey.net/
-// @version      2024-04-20
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.youtube.com/watch?v=*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @grant        GM_addStyle
-// @updateURL    https://github.com/abstraction/userscripts/blob/master/src/youtube-add-summary-btn.user.js
-// @downloadURL  https://github.com/abstraction/userscripts/blob/master/src/youtube-add-summary-btn.user.js
+// @name          Add Summarize Button
+// @version       2024-07-02
+// @description   Summarize YouTube videos using summarize.tech
+// @author        abstraction
+// @match         https://www.youtube.com/watch?v=*
+// @icon          https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant         GM_addStyle
+// @homepageURL   https://github.com/abstraction/userscripts/
+// @updateURL     https://github.com/abstraction/userscripts/blob/master/src/youtube-add-summary-btn.user.js
+// @downloadURL   https://github.com/abstraction/userscripts/blob/master/src/youtube-add-summary-btn.user.js
 // ==/UserScript==
 
 (function () {
   'use strict';
 
-  // URL to redirect
-  const redirectUrl =
-    'https://summarize.tech/' +
-    document.location.hostname +
-    document.location.pathname +
-    document.location.search;
+  const redirectUrl = `https://summarize.tech/${document.location.hostname}${document.location.pathname}${document.location.search}`;
 
-  // Function to add the button
   function addButton() {
     const subscribeButton = document.querySelector('#subscribe-button');
     if (subscribeButton && !document.querySelector('#custom-redirect-button')) {
       const button = document.createElement('button');
       button.id = 'custom-redirect-button';
       button.textContent = 'Summarize';
-      button.style.marginLeft = '8px';
-      button.style.padding = '0px 16px';
-      button.style.fontSize = '14px';
-      button.style.color = '#000';
-      button.style.backgroundColor = '#cc0000';
-      button.style.border = 'none';
-      button.style.borderRadius = '18px';
-      button.style.cursor = 'pointer';
-      button.style.height = '36px';
-      button.style.lineHeight = '36px';
-      button.style.fontWeight = 500;
-      button.style.fontFamily = '"Roboto","Arial",sans-serif';
+      Object.assign(button.style, {
+        marginLeft: '8px',
+        padding: '0px 16px',
+        fontSize: '14px',
+        color: '#000',
+        backgroundColor: '#cc0000',
+        border: 'none',
+        borderRadius: '18px',
+        cursor: 'pointer',
+        height: '36px',
+        lineHeight: '36px',
+        fontWeight: 500,
+        fontFamily: '"Roboto","Arial",sans-serif'
+      });
 
-      GM_addStyle(
-        '#custom-redirect-button:hover { background-color: #b80000 !important; }'
-      );
+      GM_addStyle('#custom-redirect-button:hover { background-color: #b80000 !important; }');
+      button.onclick = () => window.location.href = redirectUrl;
 
-      // Add click event to the button
-      button.onclick = function () {
-        window.location.href = redirectUrl;
-      };
-
-      subscribeButton.parentNode.insertBefore(
-        button,
-        subscribeButton.nextSibling
-      );
+      subscribeButton.parentNode.insertBefore(button, subscribeButton.nextSibling);
     }
   }
 
   // Try to add the button periodically in case the page content loads dynamically
-  const interval = setInterval(addButton, 2000); // Reduced interval to improve responsiveness, keep it 1000 if you're not using an extension that shifts layout
+  // FIXME - The script is unable to insert the Summarize button at times probably
+  // due to the layout shift caused by YouTube's javascript. A refresh or two usually
+  // usually fixes it but we should determine the root cause.
+  const interval = setInterval(addButton, 3000);
 
   // Clear the interval if navigating away from the page
-  window.addEventListener('beforeunload', () => {
-    clearInterval(interval);
-  });
+  window.addEventListener('beforeunload', () => clearInterval(interval));
 })();
